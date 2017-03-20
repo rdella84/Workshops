bc2d1e020dba17732ec8c820748e4e31ea1d4d2e
rdella84 <regio.della84@gmail.com>
2017-03-20 19:56:23 -0400

    adding m3

---
+++ Milestones/Milestone3/Item.cpp
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS  
+// Lib includes
+
+// inlcude Item and POS header files
+
+using namespace std;
+namespace ict{
+  // class Item implementaion
+
+
+
+
+
+}
---
+++ Milestones/Milestone3/Item.h
@@ -0,0 +1,24 @@
+#ifndef ICT_ITEM_H__
+#define ICT_ITEM_H__
+// inlcude PosIO and POS header files
+
+namespace ict{
+  // class Item
+
+
+
+
+
+
+
+
+
+
+  // end class Item
+  // operator += 
+  double operator+=(double& d, const Item& I);
+  // operator << and operator >>
+}
+
+
+#endif
